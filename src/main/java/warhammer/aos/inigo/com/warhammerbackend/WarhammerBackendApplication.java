fbb542d19cbd48bd38773f8d81890fb4b2d495a7
InigoCG <inigocg@gmail.com>
2024-04-19 20:30:03 +0200

    first commit

---
+++ src/main/java/warhammer/aos/inigo/com/warhammerbackend/WarhammerBackendApplication.java
@@ -0,0 +1,13 @@
+package warhammer.aos.inigo.com.warhammerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarhammerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarhammerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/warhammer/aos/inigo/com/warhammerbackend/WarhammerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package warhammer.aos.inigo.com.warhammerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarhammerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
